4f3175a5b88140bc96a561b7a1ea59efc26aac01
Murad Hasanli <muradhasanli888@gmail.com>
2024-11-10 23:49:46 +0400

    Add solutions

---
+++ src/FindValidMatrixGivenRowAndColumnSums.java
@@ -0,0 +1,14 @@
+public class FindValidMatrixGivenRowAndColumnSums {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int[][] ans = new int[rowSum.length][colSum.length];
+        for (int i = 0; i < rowSum.length; i++) {
+            for (int j = 0; j < colSum.length; j++) {
+                int min = Math.min(rowSum[i], colSum[j]);
+                ans[i][j] = min;
+                rowSum[i] -= min;
+                colSum[j] -= min;
+            }
+        }
+        return ans;
+    }
+}
